c0abb5300d1d094ac10381a48123e30bf7d0965e
IIAndreasII <andreas.bartilson@gmail.com>
2024-04-03 16:09:30 +0200

    Dynamically decide allocation method

--- probe-examples/dmce-probe-heatmap-k.c
+++ probe-examples/dmce-probe-heatmap-k.c
@@ -4,27 +4,35 @@
 #include <linux/slab.h>
 #include <linux/atomic.h>
 #include <linux/printk.h>
+#include <linux/panic.h>
+#include <linux/vmalloc.h>
+#include <linux/string.h>
+#include <asm/page.h>
 
+#define DMCE_BUF_SIZE (8 * DMCE_NBR_OF_PROBES)
+#if (DMCE_BUF_SIZE < PAGE_SIZE)
+#define DMCE_ALLOC() kmalloc(DMCE_BUF_SIZE, GFP_KERNEL)
+#else
+#define DMCE_ALLOC() vmalloc(DMCE_BUF_SIZE)
+#endif
 
-atomic64_t __attribute__((common)) *dmce_magic;
-
-atomic_t __attribute__((common)) alloced;
-
+atomic64_t __attribute__((common)) *dmce_buffer;
+atomic_t __attribute__((common)) dmce_buffer_allocated;
 int __attribute__((common)) nbr_probes;
 
 
-static void dmce_probe_body(unsigned int probenbr) {
-
-    if (atomic_fetch_inc(&alloced) == 0)
+static void dmce_probe_body(unsigned int probenbr)
+{
+    if (atomic_fetch_inc(&dmce_buffer_allocated) == 0)
     {
-        unsigned long num_bytes = sizeof(atomic_t) * DMCE_NBR_OF_PROBES;
-        dmce_magic = kmalloc(num_bytes, GFP_KERNEL);
-        printk("dmce_probe: dmce_magic allocated %lu bytes at 0x%px", num_bytes, dmce_magic);
+        dmce_buffer = DMCE_ALLOC();
+        memset(dmce_buffer, 0, DMCE_BUF_SIZE);
+        printk("dmce_probe: dmce_buffer allocated %lu bytes at 0x%px", (long unsigned)DMCE_BUF_SIZE, dmce_buffer);
     }
 
     // Ugly way to propagate to dmce kernel module
     nbr_probes = DMCE_NBR_OF_PROBES;
-    atomic_fetch_inc(&dmce_magic[probenbr]);
+    atomic64_fetch_inc(&dmce_magic[probenbr]);
 }
 
 #endif
